d06871c4df9248583061b023788d554146e1345d
ianpatt <ianpatt@gmail.com>
2023-11-29 18:51:23 -0800

    missed new game hook

--- skse64/Hooks_SaveLoad.cpp
+++ skse64/Hooks_SaveLoad.cpp
@@ -128,7 +128,7 @@ RelocAddr <uintptr_t> SaveGame_HookTarget_Enter(0x0060FEE0 + 0x2B3);
 RelocAddr <uintptr_t> LoadGame_HookTarget_Enter(0x006154A0 + 0x34F);
 // 691ACD40B8430FBDA081477DEB2A9948ACC235F1+11F
 RelocAddr <uintptr_t> ProcessEvents_Enter(0x006461A0 + 0x9A);
-RelocAddr <uintptr_t> NewGame_Enter(0x008E4AD0 + 0x59);
+RelocAddr <uintptr_t> NewGame_Enter(0x009427D0 + 0x59);
 RelocAddr <uintptr_t> DeleteSaveGame_Enter(0x00601DB0 + 0x77);
 RelocAddr <uintptr_t> DeleteSaveGame_Enter2(0x00601E80 + 0x17);
 
