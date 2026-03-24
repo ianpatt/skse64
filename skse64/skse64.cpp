e5544e0048ab9af10ad31fb5cc530410a10944e9
ianpatt <ianpatt@gmail.com>
2026-03-23 21:47:30 -0700

    reorg startup hooks for preinit

--- skse64/skse64.cpp
+++ skse64/skse64.cpp
@@ -26,7 +26,8 @@
 IDebugLog gLog;
 HINSTANCE g_moduleHandle = nullptr;
 
-void SKSE64_Initialize(void);
+void SKSE64_PreInit();
+void SKSE64_Initialize();
 
 // api-ms-win-crt-runtime-l1-1-0.dll
 typedef int (*__initterm_e)(_PIFV *, _PIFV *);
@@ -38,9 +39,7 @@ __get_narrow_winmain_command_line _get_narrow_winmain_command_line_Original = NU
 // runs before global initializers
 int __initterm_e_Hook(_PIFV * a, _PIFV * b)
 {
-	// could be used for plugin optional preload
-
-//	_MESSAGE("pre global init");
+	SKSE64_PreInit();
 
 	return _initterm_e_Original(a, b);
 }
@@ -50,14 +49,12 @@ char * __get_narrow_winmain_command_line_Hook()
 {
 	// the usual load time
 	
-//	_MESSAGE("post global init");
-
 	SKSE64_Initialize();
 
 	return _get_narrow_winmain_command_line_Original();
 }
 
-void SKSE64_PreInit(void)
+void InstallBaseHooks(void)
 {
 	gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\" SAVE_FOLDER_NAME "\\SKSE\\skse64.log");
 
@@ -99,81 +96,79 @@ void WaitForDebugger(void)
 	Sleep(1000 * 2);
 }
 
-static bool isInit = false;
+void SKSE64_PreInit()
+{
+	static bool runOnce = false;
+	if(runOnce) return;
+	runOnce = true;
+
+	FILETIME	now;
+	GetSystemTimeAsFileTime(&now);
+
+	_MESSAGE("SKSE64 runtime: initialize (version = %d.%d.%d %08X %08X%08X, os = %s)",
+		SKSE_VERSION_INTEGER, SKSE_VERSION_INTEGER_MINOR, SKSE_VERSION_INTEGER_BETA, RUNTIME_VERSION,
+		now.dwHighDateTime, now.dwLowDateTime, GetOSInfoStr().c_str());
+
+	_MESSAGE("imagebase = %016I64X", GetModuleHandle(NULL));
+	_MESSAGE("reloc mgr imagebase = %016I64X", RelocationManager::s_baseAddr);
+
+#ifdef _DEBUG
+	SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+
+	WaitForDebugger();
+#endif
+
+	if(!g_branchTrampoline.Create(1024 * 64))
+	{
+		_ERROR("couldn't create branch trampoline. this is fatal. skipping remainder of init process.");
+		return;
+	}
+
+	if(!g_localTrampoline.Create(1024 * 64, g_moduleHandle))
+	{
+		_ERROR("couldn't create codegen buffer. this is fatal. skipping remainder of init process.");
+		return;
+	}
+
+	// ### plugins
+
+	_MESSAGE("preinit complete");
+}
 
 void SKSE64_Initialize(void)
 {
+	static bool isInit = false;
 	if(isInit) return;
 	isInit = true;
 
-#ifndef _DEBUG
-	__try {
-#endif
+	// Add Hooks_XXX_Init calls here
+	Hooks_Debug_Init();
+	Hooks_ObScript_Init();
+	Hooks_Papyrus_Init();
+	Hooks_NetImmerse_Init();
+	Hooks_Threads_Init();
+	Hooks_Handlers_Init();
 
-		FILETIME	now;
-		GetSystemTimeAsFileTime(&now);
+	g_pluginManager.Init();
 
-		_MESSAGE("SKSE64 runtime: initialize (version = %d.%d.%d %08X %08X%08X, os = %s)",
-			SKSE_VERSION_INTEGER, SKSE_VERSION_INTEGER_MINOR, SKSE_VERSION_INTEGER_BETA, RUNTIME_VERSION,
-			now.dwHighDateTime, now.dwLowDateTime, GetOSInfoStr().c_str());
-
-		_MESSAGE("imagebase = %016I64X", GetModuleHandle(NULL));
-		_MESSAGE("reloc mgr imagebase = %016I64X", RelocationManager::s_baseAddr);
-
-#ifdef _DEBUG
-		SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
-
-		WaitForDebugger();
-#endif
-
-		if(!g_branchTrampoline.Create(1024 * 64))
-		{
-			_ERROR("couldn't create branch trampoline. this is fatal. skipping remainder of init process.");
-			return;
-		}
-
-		if(!g_localTrampoline.Create(1024 * 64, g_moduleHandle))
-		{
-			_ERROR("couldn't create codegen buffer. this is fatal. skipping remainder of init process.");
-			return;
-		}
-
-		// Add Hooks_XXX_Init calls here
-		Hooks_Debug_Init();
-		Hooks_ObScript_Init();
-		Hooks_Papyrus_Init();
-		Hooks_NetImmerse_Init();
-		Hooks_Threads_Init();
-		Hooks_Handlers_Init();
-
-		g_pluginManager.Init();
-
-		// Add Hooks_XXX_Commit calls here in the same order
-		Hooks_Debug_Commit();
-		Hooks_ObScript_Commit();
-		Hooks_Papyrus_Commit();
-		Hooks_UI_Commit();
-		Hooks_Camera_Commit();
-		Hooks_NetImmerse_Commit();
-		Hooks_Threads_Commit();
-		Hooks_Handlers_Commit();
-		Hooks_Scaleform_Commit();
-		Hooks_Gameplay_Commit();
-		Hooks_Event_Commit();
-		Hooks_SaveLoad_Commit();
-		Hooks_Data_Commit();
-		Init_CoreSerialization_Callbacks();
-		Hooks_DirectInput_Commit();
+	// Add Hooks_XXX_Commit calls here in the same order
+	Hooks_Debug_Commit();
+	Hooks_ObScript_Commit();
+	Hooks_Papyrus_Commit();
+	Hooks_UI_Commit();
+	Hooks_Camera_Commit();
+	Hooks_NetImmerse_Commit();
+	Hooks_Threads_Commit();
+	Hooks_Handlers_Commit();
+	Hooks_Scaleform_Commit();
+	Hooks_Gameplay_Commit();
+	Hooks_Event_Commit();
+	Hooks_SaveLoad_Commit();
+	Hooks_Data_Commit();
+	Init_CoreSerialization_Callbacks();
+	Hooks_DirectInput_Commit();
 		
-		FlushInstructionCache(GetCurrentProcess(), NULL, 0);
-
-#ifndef _DEBUG
-	}
-	__except(EXCEPTION_EXECUTE_HANDLER)
-	{
-		_ERROR("exception thrown during startup");
-	}
-#endif
+	FlushInstructionCache(GetCurrentProcess(), NULL, 0);
 
 	_MESSAGE("init complete");
 }
@@ -182,7 +177,7 @@ extern "C" {
 
 	void StartSKSE(void)
 	{
-		SKSE64_PreInit();
+		InstallBaseHooks();
 	}
 
 	BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
