aa05b5f79f0ea5231fd5e8187ec39c5e27767e1f
ianpatt <ianpatt@gmail.com>
2026-08-27 08:25:23 -0700

    version tick, whatsnew

--- skse64_common/skse_version.h
+++ skse64_common/skse_version.h
@@ -4,10 +4,10 @@
 // these have to be macros so they can be used in the .rc
 #define SKSE_VERSION_INTEGER		2
 #define SKSE_VERSION_INTEGER_MINOR	3
-#define SKSE_VERSION_INTEGER_BETA	0
-#define SKSE_VERSION_VERSTRING		"0, 2, 3, 0"
+#define SKSE_VERSION_INTEGER_BETA	1
+#define SKSE_VERSION_VERSTRING		"0, 2, 3, 1"
 #define SKSE_VERSION_PADDEDSTRING	"0001"
-#define SKSE_VERSION_RELEASEIDX		74
+#define SKSE_VERSION_RELEASEIDX		75
 
 #define MAKE_EXE_VERSION_EX(major, minor, build, sub)	((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) | (((build) & 0xFFF) << 4) | ((sub) & 0xF))
 #define MAKE_EXE_VERSION(major, minor, build)			MAKE_EXE_VERSION_EX(major, minor, build, 0)
@@ -62,7 +62,7 @@
 // information about the state of the game at the time of release
 #define SKSE_TARGETING_BETA_VERSION	0
 #define CURRENT_RELEASE_RUNTIME		RUNTIME_VERSION_1_7_99
-#define CURRENT_RELEASE_SKSE_STR	"2.3.0"
+#define CURRENT_RELEASE_SKSE_STR	"2.3.1"
 
 #if GET_EXE_VERSION_SUB(RUNTIME_VERSION) == RUNTIME_TYPE_BETHESDA
 #define SAVE_FOLDER_NAME "Skyrim Special Edition"
